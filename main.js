f5cd8669900d58761d9db9806a874012dceb6d90
Arkoneil17 <74445208+Arko-17@users.noreply.github.com>
2021-08-01 12:39:17 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -10,11 +10,27 @@ song1=loadSound("Megaforce.mp3");
 //song2=loadSound("DinoCharge.mp3");
 }
 function setup(){
-canvas=createCanvas(600,500);
+canvas=createCanvas(300,300);
 canvas.center();
 video=createCapture(VIDEO);
 video.hide();
+posenet=ml5.poseNet(video, modelLoaded)
+posenet.on("pose",gotPoses)
 }
 function draw(){
-image(video,0,50,600,500);
+image(video,0,0,300,300);
+}
+function modelLoaded(){
+console.log("Model has been loaded");
+}
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+        scoreleftWrist=results[0].pose.keypoints[9].score;
+        console.log("Score of Left Wrist="+scoreleftWrist);
+        leftwristX=results[0].pose.leftWrist.x;
+        leftwristY=results[0].pose.leftWrist.y;
+        rightwristX=results[0].pose.rightWrist.x;
+        
+        }
 }
