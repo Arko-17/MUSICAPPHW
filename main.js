325091a5c3ab20c569ef60f3ed79c621c666a81e
Arkoneil17 <74445208+Arko-17@users.noreply.github.com>
2021-08-14 19:37:44 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 song1="";
 song2="";
 scoreleftWrist=0;
+scorerightWrist=0;
 leftwristX=0;
 leftwristY=0;
 rightwristX=0;
 rightwristY=0;
 thestatus="";
+thestatusright="";
 function preload(){
-song1=loadSound("Megaforce.mp3");
+song1=loadSound("megaforce.mp3");
 song2=loadSound("DinoCharge.mp3");
 }
 function setup(){
@@ -23,7 +25,8 @@ image(video,0,0,300,300);
 fill("red");
 stroke("red");
 thestatus=song1.isPlaying();
-    if(scoreleftWrist>0.1){
+thestatusright=song2.isPlaying();
+    if(scoreleftWrist>0.01){
         circle(leftwristX,leftwristY,20);
         song2.stop();
             if(thestatus==false){
@@ -31,6 +34,14 @@ thestatus=song1.isPlaying();
                 document.getElementById("current").innerHTML="Megaforce Playing Now";
             }
         }
+    if(scorerightWrist>0.01){
+        circle(rightwristX,rightwristY,20);
+        song1.stop();
+            if(thestatusright==false){
+                song2.play();
+                document.getElementById("current").innerHTML="Dino Charge Playing Now";
+    }
+}
 }
 function modelLoaded(){
 console.log("Model has been loaded");
@@ -39,9 +50,12 @@ function gotPoses(results){
     if(results.length>0){
         console.log(results);
         scoreleftWrist=results[0].pose.keypoints[9].score;
+        scorerightWrist=results[0].pose.keypoints[10].score;
         console.log("Score of Left Wrist="+scoreleftWrist);
+        console.log("Score of Right Wrist="+scorerightWrist);
         leftwristX=results[0].pose.leftWrist.x;
         leftwristY=results[0].pose.leftWrist.y;
         rightwristX=results[0].pose.rightWrist.x;
+        rightwristY=results[0].pose.rightWrist.y;
         }
 }
